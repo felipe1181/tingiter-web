2c0016a17119e913cb4355c338ab570bcdf9f139
felipe henrique ferraresi <felip.ses@hotmail.com>
2019-08-08 08:47:53 -0300

    install axios and create services connection

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './login.css'; 
 import logo from '../assets/logo.svg';
 
-export default function Login(){
+export default function Login({history}){
+    const [username,setUsername] = useState('');
+
+    function handleSubmit(e){
+        e.preventDefault();
+        
+        
+
+        history.push('/main');
+    }
+
     return(
         <div className="login-container">
             <form>
                 <img src={logo} alt="Tingiter"/>
                 <input
                     placeholder="Digite um usuário do github"
+                    value={username}
+                    onChange={e =>setUsername(e.target.value)}
                 />
                 <input
                     placeholder="Digite uma senha"
